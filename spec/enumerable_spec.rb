d3a6e7d500d653d2bc16c50fc1191210ecff47b2
bellom <bellomsean@gmail.com>
2019-04-16 10:58:13 +0100

    any, map and inject added

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -17,7 +17,7 @@ describe Enumerable do
       expect(answer).to eql([10,0,25,1,12,2,6,3,13,4,17,5])
     end
   end
-  
+
   describe "#my_select" do 
     it "returns an array of all even numbers in the array passed to it" do
       expect(array.my_select {|n| n.even? }).to eql([10,12,6])
@@ -48,5 +48,25 @@ describe Enumerable do
     end
   end
 
+  describe "#my_any?" do
+    it "returns true if at least one of the elements meets the block condition" do
+      answer = array.my_any? { |element| element == 2 }
+      expect(answer).to be false
+    end
+  end
+
+  describe "#my_map" do
+		it "returns a new array with the elements modified by the block" do
+			answer = array.my_map { |element| element + 2 }
+			expect(answer).to eq([12,27,14,8,15,19])
+		end
+	end
+
+	describe "#my_inject" do
+		it "takes a parameter and yields to the block and returns the modified parameter" do
+			answer = array.my_inject(0) { |sum, element| sum += element }
+			expect(answer).to eq(83)
+		end
+	end
 
 end
